6c18d8d55c7eb3f395c861332c742fff7e070d31
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-12-02 11:26:01 +1100

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 # VPC A
 output "VPC_A_id" {
   description = "The ID of the VPC"
-  value       = module.VPC_region-1.vpc_a_vpc_id
+  value       = module.region-1.vpc_a_vpc_id
 }
 
 # Subnets
@@ -76,7 +76,7 @@ output "zREADME" {
   value = <<README
 A private RSA key has been generated and downloaded locally. The file permissions have been changed to 0600 so the key can be used immediately for SSH or scp.
 Run the below command to add this private key to the list maintained by ssh-agent so you're not prompted for it when using SSH or scp to connect to hosts with your public key.
-  ${format("ssh-add %s", module.VPC_region-1.private_key_filename)}
+  ${format("ssh-add %s", module.region-1.private_key_filename)} and ${format("ssh-add %s", module.region-2.private_key_filename)}
 The public part of the key loaded into the agent ("public_key_openssh" output) must be placed on the target system in ~/.ssh/authorized_keys.
 To SSH into the target host using this private key, you can use the below command after updating USER@HOST.
   ${format("ssh -i %s USER@HOST", module.region-1.private_key_filename)} for region-1 instances and ${format("ssh -i %s USER@HOST", module.region-2.private_key_filename)} for region-2 instances
